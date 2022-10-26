5f673efbfa40ede780714bbfed2862abb4308aaa
Sakinah Freeman <freemansakinah@gmail.com>
2022-10-25 23:53:12 -0400

    setting up trivia game

--- app.js
+++ app.js
@@ -1,35 +1,5 @@
-const apiKey = "cbbe225fe86c4eba95cd423d1daae222"
-
-const baseURL = "https://newsapi.org/v2/everything?language=en"
-
-const keyWordSearch = (keyword) => {
-    const url = `${baseURL}&q=${keyword}&apiKey=${apiKey}`
-    console.log(url)
-
-    $.ajax(url)
-    .then((data) => {
-        console.log(data)
-
-        const $main =$("main")
-        data.articles.forEach((element,index) => {
-            const div = $("<div>")
-            div.html(`<h1>${element.title}</h1>`)
-            $main.append(div)
+const url = "https://cdn.contentful.com/spaces/a4hnhuvqvorr/environments/master/entries?access_token=n3cBtKhISxqyF1VjoO77y3bIZEDvc7UUKuVgcz6d9dg&content_type=triviaQ"
+        $.ajax(url)
+        .then((data) => {
+            console.log(data)
         })
-         
-    })
-}
-
-console.log(keyWordSearch("steelers"))
-
-const categorySearch = (category) => {
-
-}
-
-const sourceSearch = (source) => {
-
-}
-
-const dateSearch = (date1, date2) => {
-
-}
